b5ff9a1c6b256d703781a483867756332b6ca36d
charlieroth <charlieroth4@gmail.com>
2024-11-04 09:09:30 +0100

    Generate accounts and auth

--- mix.exs
+++ mix.exs
@@ -25,6 +25,7 @@ defmodule Predex.MixProject do
 
   defp deps do
     [
+      {:bcrypt_elixir, "~> 3.0"},
       {:phoenix, "~> 1.7.14"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
